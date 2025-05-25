ff0064afb7f09cf94e473ec56d4f0b81a6f7f518
nek0meshi <nekmeshi@gmail.com>
2025-05-25 23:43:47 +0900

    WatchRecortList

--- src/app/watch-records/page.tsx
+++ src/app/watch-records/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import { Container } from "@/components/layout/Container";
 import { useWatchRecords } from "@/hooks/queries/useWatchRecords";
-
+import { WatchRecordBlock } from "./_components/WatchRecordBlock";
 export default function WatchRecords() {
   const { data } = useWatchRecords();
 
   return (
-    <div>
+    <Container>
       {data?.map((watchRecord) => (
-        <div key={watchRecord.id}>{watchRecord.title}</div>
+        <WatchRecordBlock key={watchRecord.id} watchRecord={watchRecord} />
       ))}
-    </div>
+    </Container>
   );
 }
