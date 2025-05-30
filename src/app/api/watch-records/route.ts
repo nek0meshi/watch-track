28ee939b00211d7616219b6e82c70d7414093853
nek0meshi <nekmeshi@gmail.com>
2025-05-30 16:32:59 +0900

    一覧取得をDBから取得する

--- src/app/api/watch-records/route.ts
+++ src/app/api/watch-records/route.ts
@@ -1,18 +1,10 @@
-import type { WatchRecord } from "@/app/schema/watch-record";
-import { range } from "@/utils/array";
-import dayjs from "dayjs";
+
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import type { WatchRecord } from "@/app/schema/watch-record";
 
 export async function GET() {
-  const watchRecords: WatchRecord[] = range(1, 30).map((i) => ({
-    id: i.toString(),
-    title: `銀河鉄道の夜${i}`,
-    watchedOn: dayjs("2021-01-01").add(i, "day").toDate(),
-    rating: 10,
-    memo: "作品の感想です。作品の感想です。作品の感想です。作品の感想です。作品の感想です。作品の感想です。作品の感想です。作品の感想です。作品の感想です。作品の感想です。作品の感想です。作品の感想です。",
-    createdAt: dayjs("2021-01-01").toISOString(),
-    updatedAt: dayjs("2021-01-01").toISOString(),
-  }));
+  const watchRecords: WatchRecord[] = await prisma.watchRecord.findMany();
 
   return NextResponse.json<WatchRecord[]>(watchRecords);
 }
--- src/app/schema/watch-record.ts
+++ src/app/schema/watch-record.ts
@@ -15,8 +15,12 @@ export const WatchRecordSchema = z.object({
   //
   rating: z.number(),
   memo: z.string(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
+  createdAt: z.string()
+    .refine((val) => dayjs(val).isValid())
+    .transform((val) => dayjs(val).toDate()),
+  updatedAt: z.string()
+    .refine((val) => dayjs(val).isValid())
+    .transform((val) => dayjs(val).toDate()),
 });
 
 export const WatchRecordsSchema = z.array(WatchRecordSchema);
